fd10d0815d081c72bbfcff982a1b7c53e8edb9c2
fabio-martin <fabiomartin2006@gmail.com>
2023-10-12 16:04:54 +0100

    Add strapi-request.mjs to test the strapi API Added node-fetch and qs Updated getReviews to get reviews from strapi CMS

---
+++ scripts/strapi-request.mjs
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch';
+import { writeFileSync } from 'node:fs';
+import qs from 'qs';
+
+const url =
+  'http://localhost:1337/api/reviews' +
+  '?' +
+  qs.stringify(
+    {
+      fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+      populate: { image: { fields: ['url'] } },
+      sort: ['publishedAt:desc'],
+      pagination: { pageSize: 6 },
+    },
+    { encodeValuesOnly: true }
+  );
+console.log('url:', url);
+const response = await fetch(url);
+const body = await response.json();
+const formatted = JSON.stringify(body, null, 2);
+// console.log(formatted);
+const file = 'scripts/strapi-response.json';
+writeFileSync(file, formatted, 'utf8');
